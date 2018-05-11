4236ac69535491c4f16051cfa6cf9b97e067317f
Martin Novák <martinnovak@outlook.com>
2018-05-11 09:00:14 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-// import './polyfills';
-
-import moment from 'moment';
-import cs from 'moment/locale/cs';
 import React from 'react';
+import moment from 'moment';
 import ReactDOM from 'react-dom';
+import cs from 'moment/locale/cs';
+
 import App from './App';
 import './index.css';
 
 moment.updateLocale('cs', cs);
 
-// import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-// registerServiceWorker();
