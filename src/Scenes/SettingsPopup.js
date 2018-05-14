ed3d54cb0481a6c5456efe2ab826d35d750919dd
Martin Novák <martinnovak@outlook.com>
2018-05-14 19:33:53 +0200

    react tabs settings

--- src/Scenes/SettingsPopup.js
+++ src/Scenes/SettingsPopup.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Popup } from '../components/Popup';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import 'react-tabs/style/react-tabs.css';
 
 export class SettingsPopup extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tabIndex: 0,
+        };
+    }
     render() {
+        const {
+            tabIndex,
+        } = this.state;
+
         const {
             filterFinishedOrders,
             handleFilterFinishedOrders,
@@ -11,27 +25,52 @@ export class SettingsPopup extends React.Component {
         return (
             <Popup
                 title="Nastavení"
+                className="popup--settings"
                 onClose={this.props.handleClose}
             >
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <div className="input-group-text">
-                            <input
-                                type="checkbox"
-                                id="filterFinishedOrders"
-                                name="filterFinishedOrders"
-                                checked={!filterFinishedOrders}
-                                onChange={handleFilterFinishedOrders}
-                            />
+                <Tabs
+                    selectedIndex={tabIndex}
+                    onSelect={(index) => this.setState({ tabIndex: index })}
+                >
+                    <TabList>
+                        <Tab
+                            selectedClassName="settings--tabs-selected"
+                        >
+                            Obecné
+                        </Tab>
+                        <Tab
+                            selectedClassName="settings--tabs-selected"
+                        >
+                            Stroje
+                        </Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text">
+                                    <input
+                                        type="checkbox"
+                                        id="filterFinishedOrders"
+                                        name="filterFinishedOrders"
+                                        checked={!filterFinishedOrders}
+                                        onChange={handleFilterFinishedOrders}
+                                    />
+                                </div>
+                            </div>
+                            <label
+                                className="form-control"
+                                htmlFor="filterFinishedOrders"
+                            >
+                                Zobrazit dokončené zakázky
+                            </label>
                         </div>
-                    </div>
-                    <label
-                        className="form-control"
-                        htmlFor="filterFinishedOrders"
-                    >
-                        Zobrazit dokončené zakázky
-                    </label>
-                </div>
+                    </TabPanel>
+
+                    <TabPanel>
+                        
+                    </TabPanel>
+                </Tabs>
             </Popup>
         );
     }
