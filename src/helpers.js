c477c5395219556affe6083f9996b8a81d0da7c8
Martin Novák <martinnovak@outlook.com>
2018-06-08 08:57:49 +0200

    create style object

--- src/helpers.js
+++ src/helpers.js
@@ -340,17 +340,15 @@ export function saveFile(path, data) {
     });
 }
 
-export function createStyleObject(width) {
-    return {
-        overflow: 'hidden',
-        whiteSpace: 'nowrap',
-        textOverflow: 'ellipsis',
-        width: `${width || 0}px`,
-        maxWidth: `${width || 0}px`,
-    };
-}
+export const createStyleObject = (width) => ({
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    width: `${width || 0}px`,
+    maxWidth: `${width || 0}px`,
+});
 
-window._mn = {
+window._debug = {
     getNetMachineTime,
     formatMinutesToTime,
     calculateOperationTime,
